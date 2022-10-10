8edb8115bc83b694697b5c0fce5f4a979ce739fe
Jai Lulla <jaylulla9@gmail.com>
2022-10-10 15:49:53 +0800

    Project Creation

---
+++ BeepBeep/BeepBeep/BeepBeepApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeepBeepApp.swift
+//  BeepBeep
+//
+//  Created by Jai Lulla  on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BeepBeepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
