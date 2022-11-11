483fd28313a29f81a0b4da304f9003e50a1c7d6a
Jai Lulla <jaylulla9@gmail.com>
2022-11-11 23:21:03 +0800

    Trial
    With Geometry Reader

---
+++ BeepBeep/BeepBeep/Views/RideTypesComponent.swift
@@ -0,0 +1,38 @@
+//
+//  RideTypesComponent.swift
+//  BeepBeep
+//
+//  Created by Jai Lulla  on 11/11/22.
+//
+
+import SwiftUI
+
+struct RideTypesComponent: View {
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack(spacing: 8) {
+                ForEach(0..<3) { item in
+                    GeometryReader { geometry in
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(.cyan)
+                                .frame(width: 150, height: 150)
+                                .border(.black)
+                                .scaleEffect(
+                                (geometry.frame(in: .global).minX / 153 >= 0.5) && (geometry.frame(in: .global).minX / 153) <= 1.2 ? 1 : 0.75)
+                        }
+                        Text("\(geometry.frame(in: .global).minX / 153)")
+                    }
+                    .frame(width: 105)
+                    .frame(maxHeight: .infinity)
+                }
+            }
+        }
+    }
+}
+
+struct RideTypesComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        RideTypesComponent()
+    }
+}
